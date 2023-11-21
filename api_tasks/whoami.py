41c8edef51c2d7225e1f72f4b42dbd52dbbce48f
Jan Banot <banot.jan@gmail.com>
2023-11-21 20:26:10 +0100

    chore: add comment

--- api_tasks/whoami.py
+++ api_tasks/whoami.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from ai_devs_task import Task
 from typing import Dict, Any, List, Tuple
 
+# TODO add typing
+
 load_dotenv()
 ai_devs_api_key: str = os.getenv("AI_DEVS_API_KEY", "")
 openai.api_key = os.getenv("OPENAI_API_KEY", "")
